345d780d581061f1e22960b239af4035a5cb18cc
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-07-08 13:21:38 +0545

    Revert "update"
    This reverts commit c1f6483402ab7a1e1908159c3c365567f4a85837.

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,6 +29,7 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
+import vue from "./tech/vue.png";
 
 export {
   logo,
@@ -59,4 +60,5 @@ export {
   carrent,
   jobit,
   tripguide,
+  vue,
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,8 +44,8 @@ const Navbar = () => {
         >
           <img src={logo} alt='logo' className='w-9 h-9 object-contain' />
           <p className='text-white text-[18px] font-bold cursor-pointer flex '>
-            Vishwa &nbsp;
-            <span className='sm:block hidden'> | Thv</span>
+            Aayush &nbsp;
+            <span className='sm:block hidden'> Adhikari</span>
           </p>
         </Link>
 

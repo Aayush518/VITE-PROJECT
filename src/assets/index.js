be8292c2ccb175cac17fd9d54afd94490c9e8f61
Aayush Adhikari <adhikariaayush37@gmail.com>
2023-07-08 12:05:45 +0545

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -29,7 +29,7 @@ import tesla from "./company/tesla.png";
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
 import tripguide from "./tripguide.png";
-import vue from "./vue.png";
+import vue from "./tech/vue.png";
 
 export {
   logo,
